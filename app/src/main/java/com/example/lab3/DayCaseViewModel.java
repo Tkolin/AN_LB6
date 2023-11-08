f8f4339a3015eed57947df6736d56d20a8c5c562
Dmitry <96141422+Tkolin@users.noreply.github.com>
2023-11-08 21:04:23 +0700

    Update DayCaseViewModel.java
    1s

--- app/src/main/java/com/example/lab3/DayCaseViewModel.java
+++ app/src/main/java/com/example/lab3/DayCaseViewModel.java
@@ -8,20 +8,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DayCaseViewModel extends ViewModel {
+    // Список
     private MutableLiveData<List<DayCase>> dayCasesLiveData = new MutableLiveData<>();
 
+    // Метод для получения списка
     public LiveData<List<DayCase>> getDayCases() {
         return dayCasesLiveData;
     }
 
+    // Метод Добавление указаной задачи в список
     public void addDayCase(DayCase dayCase) {
         List<DayCase> dayCases = dayCasesLiveData.getValue();
         if (dayCases == null) {
             dayCases = new ArrayList<>();
         }
-        dayCases.add(dayCase);
-        dayCasesLiveData.setValue(dayCases);
+        dayCases.add(dayCase); // Добавление значения в список
+        dayCasesLiveData.setValue(dayCases); // Установка списка в dayCasesLiveData
     }
+     // Метод Добавление указаной задачи из списка
     public void removeDayCase(int position) {
         List<DayCase> dayCases = dayCasesLiveData.getValue();
         if (dayCases != null && position >= 0 && position < dayCases.size()) {
